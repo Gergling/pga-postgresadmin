ed28651fee3d044072ae2147e1d25671ea202755
Greg <gregmichaeldavies@gmail.com>
2026-01-13 16:38:27 +0000

    feat: Reconfigured to prevent refetching on focus by default.

--- src/renderer/libs/react-query/Provider.tsx
+++ src/renderer/libs/react-query/Provider.tsx
@@ -1,7 +1,15 @@
 import { PropsWithChildren } from "react";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // 5 minutes
+      refetchOnWindowFocus: false,
+      // retry: false,
+    },
+  },
+});
 
 export const AppQueryProvider = ({ children }: PropsWithChildren) =>
   <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;

319a44a0bd836e9cc33657c3f8dc644c33cf5ef7
Greg <gregmichaeldavies@gmail.com>
2026-03-11 16:04:54 +0000

    feat: Replace AppThemeProvider with AppThemeOverrideProvider in AppContextProvider

--- src/renderer/app/AppContextProvider.tsx
+++ src/renderer/app/AppContextProvider.tsx
@@ -1,18 +1,18 @@
 import { PropsWithChildren } from "react";
-import { AppThemeProvider } from '@gergling/ui-components';
 import { IpcContextProvider } from "../shared/ipc/Provider";
 import { NestedProviders } from "../shared/common/components/NestedProviders";
 import { NavigationProvider } from "../shared/navigation";
 import { AppLocalisationProvider } from "../libs/mui";
 import { AppQueryProvider } from "../libs/react-query";
 import { DiaryProvider } from "../features/diary";
+import { AppThemeOverrideProvider } from "../shared/theme";
 
 export const AppContextProvider = ({ children }: PropsWithChildren) => {
   return (
     <NestedProviders components={[
       IpcContextProvider,
       AppQueryProvider,
-      AppThemeProvider,
+      AppThemeOverrideProvider,
       AppLocalisationProvider,
       DiaryProvider,
       NavigationProvider,

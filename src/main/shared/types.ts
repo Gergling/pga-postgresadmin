0f598b11ed16fe330cac5ff16d9026115b056d05
Greg <gregmichaeldavies@gmail.com>
2026-03-18 21:00:16 +0000

    tech: Removed superfluous types.

--- src/main/shared/types.ts
+++
@@ -1,9 +0,0 @@
-import { TimelineStatus } from "../../shared/features/crm";
-
-export type DbTimeline = {
-  start?: number | TimelineStatus;
-  end?: number | TimelineStatus;
-};
-
-export type ReadList<T extends object, U = unknown> = (...args: U[]) => Promise<T[]>;
-export type UpdateItem<T extends object> = (id: string, newData: Partial<T>) => Promise<T>;

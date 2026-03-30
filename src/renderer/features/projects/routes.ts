f506851b4a0352851add815895ab61214515e1dc
Greg <gregmichaeldavies@gmail.com>
2026-03-30 13:13:45 +0100

    feat: add project routes

---
+++ src/renderer/features/projects/routes.ts
@@ -0,0 +1,28 @@
+import { createElement } from "react";
+import { UiNavigationConfigItem } from "@/renderer/shared/navigation";
+import { runeFactory } from "../svg-viewer/components";
+import { ProjectDetail, ProjectsList } from "./components";
+
+export const PROJECT_ROUTES: UiNavigationConfigItem = {
+  icon: runeFactory('Projects'),
+  label: 'Projects',
+  path: 'projects',
+  element: ProjectsList,
+  children: [
+    {
+      label: 'Project List',
+      path: '',
+      icon: runeFactory('Project'),
+      element: () => 'DONG',
+    },
+    {
+      label: '(Unnamed Project)', // Find a way to omit this.
+      path: ':projectName',
+      icon: runeFactory('Project'), // Find a way to omit this.
+      element: (props) => {
+        console.log('props', props)
+        return createElement(ProjectDetail, { projectName: 'john' });
+      },
+    }
+  ]
+};

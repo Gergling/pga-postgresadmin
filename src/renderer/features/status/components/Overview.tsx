3f826f7eb13b7217a507eb282006b6c357bfd2cb
Greg <gregmichaeldavies@gmail.com>
2026-04-05 13:44:59 +0100

    feat: Status overview layout.

---
+++ src/renderer/features/status/components/Overview.tsx
@@ -0,0 +1,7 @@
+import { Slab } from "@/renderer/shared/base";
+
+export const StatusOverview = () => {
+  return <Slab>
+    Status Coming Soon to an interface near you.
+  </Slab>
+};
--- src/renderer/views/Root.tsx
+++ src/renderer/views/Root.tsx
@@ -1,3 +1,4 @@
+// TODO: Move to app level, along with overall navigation routing.
 import { useTheme } from "@gergling/ui-components";
 import { PropsWithChildren, useEffect } from "react";
 import { Outlet, useLocation } from "react-router-dom";
@@ -7,8 +8,9 @@ import { ErrorBoundary } from "../shared/common/components/ErrorBoundary";
 import { DiaryDrawer, useDiary } from "../features/diary";
 import { useTaskNavigation } from "../features/tasks/hooks/navigation";
 import { PhraseDuJour } from "../features/banners";
-import { HeaderBanner } from "../features/banners/components/Header";
 import { useProjectNavigation } from "../features/projects";
+import { Box } from "@mui/material";
+import { StatusOverview } from "../features/status";
 // import type {} from '@mui/x-date-pickers/themeAugmentation';
 
 const useApp = () => {
@@ -37,27 +39,60 @@ const useApp = () => {
 
 const NavigationFallback = ({ children }: PropsWithChildren) => <>{children} <a style={{ color: 'white' }} href="/">Return Home</a></>;
 
+const STATUS_HEIGHT = 1.1;
+const STATUS_PADDING = 0.5;
+const MAIN_BOTTOM = STATUS_HEIGHT + (STATUS_PADDING * 2);
+const LAYOUT_CONFIG = {
+  STATUS_HEIGHT,
+  STATUS_PADDING,
+  MAIN_BOTTOM,
+} as const;
+type LayoutConfig = typeof LAYOUT_CONFIG;
+const LAYOUT_MEASUREMENTS = Object.entries(LAYOUT_CONFIG).reduce(
+  (acc, [key, value]) => ({ ...acc, [key]: `${value}rem` }),
+  {} as Record<keyof LayoutConfig, string>
+);
+
 export const RootView = () => {
   const { pathname, search } = useApp();
 
   return (
     <ErrorBoundary fallback={<NavigationFallback>Something bad has happened.</NavigationFallback>}>
-      <DevModeOverlay />
+      <Box sx={{
+        overflow: 'auto',
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: LAYOUT_MEASUREMENTS.MAIN_BOTTOM,
+      }}>
+        <DevModeOverlay />
 
-      <ErrorBoundary fallback={<NavigationFallback>Navigation has failed.</NavigationFallback>}>
-        <NavigationBreadcrumbs />
-      </ErrorBoundary>
+        <ErrorBoundary fallback={<NavigationFallback>Navigation has failed.</NavigationFallback>}>
+          <NavigationBreadcrumbs />
+        </ErrorBoundary>
 
-      {/* <HeaderBanner /> */}
-      <PhraseDuJour />
+        {/* <HeaderBanner /> */}
+        <PhraseDuJour />
 
-      <ErrorBoundary fallback={<NavigationFallback>Something has gone wrong with the Root outlet at {pathname + search}.</NavigationFallback>}>
-        <Outlet />
-      </ErrorBoundary>
+        <ErrorBoundary fallback={<NavigationFallback>Something has gone wrong with the Root outlet at {pathname + search}.</NavigationFallback>}>
+          <Outlet />
+        </ErrorBoundary>
 
-      <ErrorBoundary fallback={<NavigationFallback>Diary drawer has failed.</NavigationFallback>}>
-        <DiaryDrawer />
-      </ErrorBoundary>
+        <ErrorBoundary fallback={<NavigationFallback>Diary drawer has failed.</NavigationFallback>}>
+          <DiaryDrawer />
+        </ErrorBoundary>
+      </Box>
+      <Box sx={{
+        position: 'fixed',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        height: LAYOUT_MEASUREMENTS.STATUS_HEIGHT,
+        padding: LAYOUT_MEASUREMENTS.STATUS_PADDING,
+      }}>
+        <StatusOverview />
+      </Box>
     </ErrorBoundary>
   );
 };

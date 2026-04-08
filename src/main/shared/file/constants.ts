d9358e62252378f19d526a0c9d254ed52f3442c2
Greg <gregmichaeldavies@gmail.com>
2026-04-08 10:21:03 +0100

    fix: Fixed project root constant.

--- src/main/shared/file/constants.ts
+++ src/main/shared/file/constants.ts
@@ -1,7 +1,6 @@
 import { resolve } from 'node:path';
 import { log } from '../logging';
 
-export const PROJECT_ROOT = resolve(__dirname, '../../');
+export const PROJECT_ROOT = resolve(__dirname, '../../../../');
 
-log('PROJECT_ROOT:', 'info')
-log(PROJECT_ROOT, 'info')
+log(`PROJECT_ROOT: ${PROJECT_ROOT}`, 'info')

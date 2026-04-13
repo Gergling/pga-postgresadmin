4aa2c1d0789f23a6cf16d473f96419097c20ca9c
Greg <gregmichaeldavies@gmail.com>
2026-04-13 18:32:07 +0100

    feat: Updated category functions.

--- src/shared/features/recency/utilities/category.ts
+++ src/shared/features/recency/utilities/category.ts
@@ -2,10 +2,19 @@ import { Temporal } from "@js-temporal/polyfill";
 import { TemporalGranularity } from "../config";
 import { TemporalRelativeCategory } from "../types";
 
+export const getTemporalRelativeSinceOverlap = (
+  granularity: TemporalGranularity,
+  now: Temporal.ZonedDateTime
+): Temporal.Duration => now.since({ [granularity]: 1 });
+export const getTemporalRelativeOverlapThreshold = (
+  duration: Temporal.Duration,
+  now: Temporal.ZonedDateTime
+): Temporal.ZonedDateTime => now.subtract(duration);
+
 /**
  * Let's take a series of dates in chronological order:
  |-----------------------------------------------------------------
- | Before Yesterday
+ | Older: Before Yesterday
  |---Prior-Threshold-----------------------------------------------
  | Prior: Yesterday but before 24h ago (before `since` starts).
  |---Overlap-Threshold---------------------------------------------
@@ -21,21 +30,23 @@ import { TemporalRelativeCategory } from "../types";
  * @param since The duration passed between now and the time period for the
  * granularity.
  * @param granularity The duration property we're using for reference.
- * @param prior The threshold that 
+ * @param prior The threshold at the beginning of the previous time period.
  * @returns 
  */
 
 export const getTemporalRelativeCategory = (
   date: Temporal.ZonedDateTime,
-  prior: Temporal.ZonedDateTime,
-  since: Temporal.Duration,
-  granularity: TemporalGranularity,
-  window: Temporal.ZonedDateTime,
-  now?: Temporal.ZonedDateTime
+  { prior, overlap, window, now }: {
+    prior: Temporal.ZonedDateTime,
+    overlap: Temporal.ZonedDateTime,
+    window: Temporal.ZonedDateTime,
+    now: Temporal.ZonedDateTime,
+  }
 ): TemporalRelativeCategory => {
-  if (prior.epochMilliseconds < date.epochMilliseconds) return 'older';
-  if (now && now.epochMilliseconds < date.epochMilliseconds) return 'future';
-  if (since[granularity] > 0) return 'prior';
-  if (window.epochMilliseconds < date.epochMilliseconds) return 'window';
-  return 'overlap';
+  const ms = date.epochMilliseconds;
+  if (ms > now.epochMilliseconds) return 'future';
+  if (ms > window.epochMilliseconds) return 'window';
+  if (ms > overlap.epochMilliseconds) return 'overlap';
+  if (ms > prior.epochMilliseconds) return 'prior';
+  return 'older';
 };

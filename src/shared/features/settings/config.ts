ddbc1ed180b0b045b10e77020795a0eb732b96ed
Greg <gregmichaeldavies@gmail.com>
2026-04-18 18:33:22 +0100

    feat: Added application settings format.

---
+++ src/shared/features/settings/config.ts
@@ -0,0 +1,21 @@
+import zod from 'zod';
+
+export const APPLICATION_SETTINGS_SCHEMA = zod.object({
+  firebase: zod.object({
+    production: zod.any(),
+    development: zod.any(),
+  }),
+  gemini: zod.object({
+    apiKey: zod.string(),
+  }),
+  gmail: zod.object({
+    host: zod.string(),
+    user: zod.string(),
+    pass: zod.string(),
+  }),
+  projects: zod.object({
+    path: zod.string(),
+  }),
+});
+
+export type ApplicationSettings = zod.infer<typeof APPLICATION_SETTINGS_SCHEMA>;

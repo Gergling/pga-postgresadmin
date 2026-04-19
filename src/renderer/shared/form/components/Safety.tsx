0ba155ca036adcb303b9e890bffdf7aeaea91ecb
Greg <gregmichaeldavies@gmail.com>
2026-04-19 18:18:40 +0100

    feat: Gave safety some layout improvements.

--- src/renderer/shared/form/components/Safety.tsx
+++ src/renderer/shared/form/components/Safety.tsx
@@ -1,12 +1,20 @@
-// Simple control wrapper which defaults to an uneditable data display, unless explicitly switched to edit mode.
+// Simple control wrapper which defaults to an uneditable data display, unless
+// explicitly switched to edit mode.
 // This is to provide a "this doesn't action but FYI" feel.
 
 import { Check, Edit } from "@mui/icons-material";
-import { IconButton } from "@mui/material";
+import { Grid, GridProps, IconButton } from "@mui/material";
 import { useState } from "react";
+import { WX } from "../types";
 
 type SafetyControlProps = {
+  /**
+   * The control node. Usually a form field.
+   */
   control: React.ReactNode;
+  /**
+   * The display node. Can be anything, but usually just the value of the item.
+   */
   display: React.ReactNode;
   /**
    * If true, shows the control, otherwise the display.
@@ -21,18 +29,38 @@ type SafetyControlProps = {
    * The icon that represents switching to display mode.
    */
   doneIcon?: React.ReactNode;
+  /**
+   * Extended child props.
+   */
+  wx?: WX<GridProps, {
+    button: WX<GridProps>;
+    data: WX<GridProps>;
+  }>;
 };
+
+// TODO: Wrap the display and control so that the state of editability is more
+// clearly expressed.
+
 /**
  * Switches between two modes: control and display.
  */
-export const SafetyControl = ({ control, display, doneIcon, editIcon, ...props }: SafetyControlProps) => {
+export const SafetyControl = ({
+  control, display, doneIcon, editIcon, wx,
+  ...props
+}: SafetyControlProps) => {
   const [editable, setEditable] = useState(props.editable);
   const handleToggle = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     setEditable(!editable);
   };
-  return <>
-    {editable ? control : display}
-    <IconButton onClick={handleToggle}>{editable ? doneIcon ?? <Check /> : editIcon ?? <Edit />}</IconButton>
-  </>;
+  return <Grid container spacing={2} {...wx?.props}>
+    <Grid size={'grow'} {...wx?.children?.data}>
+      {editable ? control : display}
+    </Grid>
+    <Grid size="auto" {...wx?.children?.button}>
+      <IconButton onClick={handleToggle}>
+        {editable ? doneIcon ?? <Check /> : editIcon ?? <Edit />}
+      </IconButton>
+    </Grid>
+  </Grid>;
 };

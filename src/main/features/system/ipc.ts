61a1a01f1497009b0631c04af0f7edda77de9666
Greg <gregmichaeldavies@gmail.com>
2026-05-07 19:44:07 +0100

    refactor: System features now use trpc.

--- src/main/features/system/ipc.ts
+++ src/main/features/system/ipc.ts
@@ -1,43 +1,17 @@
-import { observable } from '@trpc/server/observable';
-import { FirebaseDatabaseStatus } from '@/shared/lib/firebase';
 import { tRPC } from "@/main/config";
 import {
   getDatabaseStatus,
   getFirebaseDb
 } from "@/main/libs/firebase";
 import { checkResources } from "./crud";
-import { CheckResourceResponse } from "./types";
 
 export const systemRouter = tRPC.router({
-  checkDatabaseStatus: tRPC.procedure.subscription(
-    () => observable<FirebaseDatabaseStatus>((emit) => {
-      const emitDatabaseStatus = async () => {
-        const db = await getFirebaseDb(true);
-        const status = getDatabaseStatus(db);
-        emit.next(status);
-      };
-      const timer = setInterval(emitDatabaseStatus, 5000);
-
-      emitDatabaseStatus();
-
-      return () => {
-        clearInterval(timer);
-      };
-    })
-  ),
-  checkResourceSubscription: tRPC.procedure.subscription(
-    () => observable<CheckResourceResponse>((emit) => {
-      const emitResources = async () => {
-        const resources = await checkResources();
-        emit.next(resources);
-      }
-      const timer = setInterval(emitResources, 5000);
-
-      emitResources();
-
-      return () => {
-        clearInterval(timer);
-      };
-    }),
-  ),
+  check: tRPC.procedure.query(async () => {
+    const firestoreDb = await getFirebaseDb(true);
+    const db = getDatabaseStatus(firestoreDb);
+    return {
+      db,
+      resources: await checkResources(),
+    };
+  }),
 });

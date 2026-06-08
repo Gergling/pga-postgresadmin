e6508e889795442578a75f5562496cfa84002e74
Greg <gregmichaeldavies@gmail.com>
2026-06-09 00:33:43 +0100

    feat(explorer): add createUnitTest subscription to explorer router

--- src/main/features/explorer/ipc.ts
+++ src/main/features/explorer/ipc.ts
@@ -1,6 +1,8 @@
 import z from 'zod';
+import { observable } from '@trpc/server/observable';
 import { tRPC } from '@/main/config';
 import { findLockingProcesses, listContents } from './extractors';
+import { emitUnitTest, GenerateUnitTestUpdateProps } from './transformers';
 
 export const explorerRouter = tRPC.router({
   list: tRPC.procedure.input(z.string()).query(
@@ -9,4 +11,13 @@ export const explorerRouter = tRPC.router({
   locks: tRPC.procedure.input(z.string()).query(
     ({ input }) => findLockingProcesses(input),
   ),
+  createUnitTest: tRPC.procedure
+    .input(z.string())
+    .subscription(
+      ({ input: sourceFilePath }) => observable<
+        GenerateUnitTestUpdateProps, GenerateUnitTestUpdateProps
+      >((emit) => {
+        emitUnitTest({ emit, sourceFilePath });
+      })
+    ),
 });

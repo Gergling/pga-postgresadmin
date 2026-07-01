92198e28455b98777734ad14389861803f251a6c
Greg <gregmichaeldavies@gmail.com>
2026-07-01 15:27:58 +0100

    feat(app): handle uncaught errors in React root

--- src/renderer/app/App.tsx
+++ src/renderer/app/App.tsx
@@ -12,7 +12,12 @@ if (appElement === null) {
   throw new Error("Element with ID 'app' not found in the DOM.");
 }
 
-const root = createRoot(appElement);
+const root = createRoot(appElement, {
+  onUncaughtError: (error, errorInfo) => {
+    console.error('Uncaught error:', error, errorInfo);
+    return <>Something went so badly wrong that it had to be caught at `createRoot`.</>;
+  },
+});
 root.render(
   <React.StrictMode>
     <App />

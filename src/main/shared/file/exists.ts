0ae3eb15778163439601ec1f2a0d8c54c6a10764
Greg <gregmichaeldavies@gmail.com>
2026-07-19 10:42:11 +0100

    refactor(file): refactor fileExists to handle unreadable files

--- src/main/shared/file/exists.ts
+++ src/main/shared/file/exists.ts
@@ -1,13 +1,16 @@
-import { access } from "node:fs";
-import { promisify } from "node:util";
+import { access, constants } from "node:fs";
+// import { promisify } from "node:util";
 
-const accessAsync = promisify(access);
+// const accessAsync = promisify(access);
 
-export const fileExists = async (path: string) => {
-  try {
-    await accessAsync(path);
-    return true;
-  } catch {
-    return false;
-  }
-};
+// TODO: IDK if this is useful here. I ended up not using it because it needed
+// to identify when a file's metadata was unreadable.
+
+export const fileExists = (path: string) => new Promise<boolean>((resolve) => {
+  access(path, constants.F_OK, (err) => {
+    if (err?.code === 'ENOENT' || err?.code === 'ENOTDIR') {
+      return resolve(false);
+    }
+    return resolve(true);
+  });
+});

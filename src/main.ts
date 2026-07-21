5e3a49dfcee2dd88b36610795be6fdded2335d3d
Greg <gregmichaeldavies@gmail.com>
2026-07-21 17:10:21 +0100

    refactor(main): refactor: remove redundant initialise and add path documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { app } from 'electron';
 import squirrelStartup from 'electron-squirrel-startup';
-import { initialise, setupElectronEventHandlers } from '@/main/app';
+import { setupElectronEventHandlers } from '@/main/app';
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (squirrelStartup) {
@@ -8,6 +8,11 @@ if (squirrelStartup) {
   process.exit(0);
 }
 
-initialise();
-
 setupElectronEventHandlers(app);
+
+/**
+ * Path Type 	Code Snippet	Use Case
+Root App Path	app.getAppPath()	The root folder containing your package.json.
+User Data	app.getPath('userData')	Storing settings, databases, or logs.
+Executable Path	app.getPath('exe')	The directory where the actual .exe or binary lives.
+ */

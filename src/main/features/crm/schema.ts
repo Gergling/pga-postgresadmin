4c8760dc0d61c7eaa7eab459eb97b8cdfa1ac3b4
Greg <gregmichaeldavies@gmail.com>
2026-07-21 17:07:43 +0100

    feat(crm): add CRM repositories

--- src/main/features/crm/schema.ts
+++ src/main/features/crm/schema.ts
@@ -1,6 +1,30 @@
 import { createSchema } from "../../../shared/lib/typesaurus/utilities";
 import { CrmDbSchema } from "../../../shared/features/crm";
+import { createProcrastinatedRepo } from "@/main/libs/firebase";
+import {
+  CrmCompany,
+  CrmEmployment,
+  CrmPerson,
+  crmCompanySchema,
+  crmEmploymentSchema,
+  crmPersonSchema
+} from "@/shared/features/crm";
 
 export const crmDb = createSchema<CrmDbSchema>(
   ['companies', 'people', 'employments']
+);
+
+export const crmPersonRepo = createProcrastinatedRepo<CrmPerson>(
+  'people',
+  crmPersonSchema
+);
+
+export const crmCompanyRepo = createProcrastinatedRepo<CrmCompany>(
+  'companies',
+  crmCompanySchema
+);
+
+export const crmEmploymentRepo = createProcrastinatedRepo<CrmEmployment>(
+  'employments',
+  crmEmploymentSchema
 );

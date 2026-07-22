3e45a6a419143385425696e0ccf76e9e388bba2e
Greg <gregmichaeldavies@gmail.com>
2026-07-22 09:54:25 +0100

    feat(system): Log CPU and memory band details when compute status is red

--- src/main/features/system/transformers/report.ts
+++ src/main/features/system/transformers/report.ts
@@ -30,14 +30,12 @@ const getComputeMessage = (
 ) => {
   if (current === previous) return '';
   const currentColourCode = mapSystemComputeBandToAnsiCode(current);
-  if (previous === undefined) {
-    return [
-      `Initialised System Monitoring Compute Band: `,
-      ANSI_COLOUR_MAP[currentColourCode],
-      current,
-      ANSI_COLOUR_MAP.reset,
-    ].join('');
-  }
+  if (previous === undefined) return [
+    `Initialised System Monitoring Compute Band: `,
+    ANSI_COLOUR_MAP[currentColourCode],
+    current,
+    ANSI_COLOUR_MAP.reset,
+  ].join('');
   const previousColourCode = mapSystemComputeBandToAnsiCode(previous);
   return [
     `Compute band changed from `,
@@ -54,14 +52,27 @@ const getComputeMessage = (
 const tick = async () => {
   const cpu = getCpuUsage();
   const memory = getTrendingFreeMemory();
-  const band = getSystemBands(cpu, memory).compute;
+  const { compute: band, cpu: cpuBand, memory: memoryBand } = getSystemBands(cpu, memory);
+  // console.log(cpu, cpuBand, memory, memoryBand)
   const previous = data.compute.length
     ? medianDiscrete(data.compute, SYSTEM_COMPUTE_COLOR_ORDER)
     : undefined;
   data.compute = [...data.compute, band].slice(-4);
   const current = medianDiscrete(data.compute, SYSTEM_COMPUTE_COLOR_ORDER);
   const message = getComputeMessage(current, previous);
-  if (message) log(message);
+  if (message) {
+    if (current === 'red') {
+      log([
+        message, ANSI_COLOUR_MAP.cyan,
+        ', CPU: ', ANSI_COLOUR_MAP[mapSystemComputeBandToAnsiCode(cpuBand)],
+        cpuBand, ANSI_COLOUR_MAP.cyan,
+        ', Memory: ', ANSI_COLOUR_MAP[mapSystemComputeBandToAnsiCode(memoryBand)],
+        memoryBand, ANSI_COLOUR_MAP.cyan,
+      ].join(''));
+    } else {
+      log(message);
+    }
+  }
   await wait(5000);
   tick();
 };

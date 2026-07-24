44f536550b627d825eace19cbcf472e852473086
Greg <gregmichaeldavies@gmail.com>
2026-07-24 16:23:20 +0100

    refactor(nedb): Update SerialisationEnvelope constraint in setupLocalNeDb

--- src/main/libs/nedb/utilities/setup.ts
+++ src/main/libs/nedb/utilities/setup.ts
@@ -1,11 +1,12 @@
 import { SerialisationEnvelope } from '@/shared/schema';
 import { instantiateNeDbWrapper } from './instantiator';
+import { ZodRawShape } from 'zod';
 
 export const setupBasicNeDb = <T extends object>(
   collectionName: string
 ) => instantiateNeDbWrapper<T>(collectionName);
 
-export const setupLocalNeDb = <T extends SerialisationEnvelope<unknown>>(
+export const setupLocalNeDb = <T extends SerialisationEnvelope<ZodRawShape>>(
   collectionName: string,
 ) => {
   const local = setupBasicNeDb<T>(collectionName);

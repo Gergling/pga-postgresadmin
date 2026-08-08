4e6a28f55b4c8b43fbc5082f1980756ff8b939bc
Greg <gregmichaeldavies@gmail.com>
2026-08-08 15:49:33 +0100

    feat: Diary update function.

--- src/main/features/diary/db/update.ts
+++ src/main/features/diary/db/update.ts
@@ -7,6 +7,17 @@
 // "processing" -> "rejected"
 // Update things like the task ids.
 
+import {
+  DiaryEntrySerialisation,
+  diaryEntrySerialisationSchema
+} from "@/shared/features/diary";
+import { diaryDb } from "../schema";
+
 // TODO: All committed items with a type of undefined id will be brought into a
 // "queue" for processing. If no relevant ids are found, it will be put in as an
 // empty array.
+
+export const updateDiaryEntry = async (entry: DiaryEntrySerialisation) => {
+  const envelope = diaryEntrySerialisationSchema.parse(entry);
+  await diaryDb.update({ id: envelope.id }, envelope);
+};
--- src/main/features/diary/ipc.ts
+++ src/main/features/diary/ipc.ts
@@ -3,11 +3,18 @@ import {
   diaryEntrySerialisationSchema
 } from "@/shared/features/diary";
 import { tRPC } from "@/main/config";
-import { createNewDiaryEntry, fetchRecentDiaryEntries } from "./db";
+import {
+  createNewDiaryEntry,
+  fetchRecentDiaryEntries,
+  updateDiaryEntry
+} from "./db";
 
 export const diaryRouter = tRPC.router({
   create: tRPC.procedure.input(diaryEntrySerialisationSchema).mutation(
     ({ input }): Promise<DiaryEntrySerialisation> => createNewDiaryEntry(input),
   ),
   fetchRecent: tRPC.procedure.query(() => fetchRecentDiaryEntries()),
+  update: tRPC.procedure.input(diaryEntrySerialisationSchema).mutation(
+    ({ input }): Promise<void> => updateDiaryEntry(input),
+  ),
 });

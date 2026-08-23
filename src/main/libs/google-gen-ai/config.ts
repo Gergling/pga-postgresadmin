db858627ebfb97f053b0f92fde25073e583c7b6f
Greg <gregmichaeldavies@gmail.com>
2026-08-23 16:50:50 +0100

    feat(google-gen-ai): integrate logging and improve error handling

--- src/main/libs/google-gen-ai/config.ts
+++ src/main/libs/google-gen-ai/config.ts
@@ -6,7 +6,7 @@ import { catchGeminiError, fetchGoogleGenAIInstance } from "./utilities";
 
 export const googleLanguageModelConfig = languageModelSourceLevelConfig({
   source: 'google',
-  models: async (preferred, excluded) => {
+  models: async () => {
     const ai = await fetchGoogleGenAIInstance();
     const models = await ai.models.list();
     // TODO: Needs an iteration system for the next page at some point.
@@ -14,7 +14,6 @@ export const googleLanguageModelConfig = languageModelSourceLevelConfig({
       name, temperature, ...model
     }) => {
       if (name === undefined) return acc;
-      if (excluded.includes(name)) return acc;
       const thinking = !!model.thinking;
       return [
         ...acc,
@@ -29,17 +28,26 @@ export const googleLanguageModelConfig = languageModelSourceLevelConfig({
       ];
     }, [] as LanguageModelSourceLevelProps[]);
   },
-  generate: async ({ model, prompt, schema, temperature }) => {
-    const ai = await fetchGoogleGenAIInstance();
+  generate: ({
+    model, prompt, schema, temperature, logApi: { log }
+  }) => log(`Generating gemini model response`, async (logApi) => {
+    const ai = await logApi.log(
+      `Fetching gemini instance`,
+      fetchGoogleGenAIInstance
+    );
     try {
-      const response = await ai.models.generateContent({
-        contents: prompt,
-        model,
-        config: {
-          temperature,
-          responseJsonSchema: schema?.toJSONSchema(),
-        },
-      });
+      const response = await logApi.log(
+        `Running generateContent`,
+        async () => ai.models.generateContent({
+          contents: prompt,
+          model,
+          config: {
+            temperature,
+            responseJsonSchema: schema?.toJSONSchema(),
+          },
+        }), { showSummaryChildren: true }
+      );
+
       if (response.text === undefined) throw new Error(`Model generated an undefined response. This will need handling.`);
       // TODO: Need a separate status output for a response which has succeeded
       // in generating the output, but may need to try again if it didn't follow
@@ -51,7 +59,7 @@ export const googleLanguageModelConfig = languageModelSourceLevelConfig({
         status: 'success',
       };
     } catch (error) {
-      return catchGeminiError(error, model);
+      return catchGeminiError(error, model, logApi);
     }
-  },
+  }),
 });
--- src/main/libs/google-gen-ai/utilities/errors.ts
+++ src/main/libs/google-gen-ai/utilities/errors.ts
@@ -1,74 +1,96 @@
-import {
-  LanguageModelSourceLevelResponse,
-  log,
-} from "@/main/shared";
 import { ApiError } from "@google/genai";
 import z from "zod";
+import { LanguageModelSourceLevelResponse } from "@/shared/features/llm";
+import {
+  log,
+  LogApi,
+} from "@/main/shared";
 
 const ApiErrorInfoSchema = z.object({
-  message: z.string(),
   status: z.number(),
+});
+const ApiErrorInfoSchemaWithMessage = ApiErrorInfoSchema.extend({
+  message: z.string(),
 });
 
 export const catchGeminiError = (
   caught: unknown,
   model: string,
+  { setStatus }: LogApi
 ): LanguageModelSourceLevelResponse => {
-  if (typeof caught !== 'object' || caught === null) throw caught;
-  
+  if (typeof caught !== 'object' || caught === null) {
+    setStatus('error', 'Caught error is not an object.');
+    throw caught;
+  }
+
   const apiErrorInfo = ApiErrorInfoSchema.safeParse(caught);
   if (apiErrorInfo.success) {
-    const apiError = new ApiError(apiErrorInfo.data);
-    switch(apiError.status) {
+    switch (apiErrorInfo.data.status) {
       case 503: return {
         canRetry: true,
         model,
         status: 'traffic',
       };
-      case 429: 
-      try {
-        const parsed = JSON.parse(apiError.message);
-        const reduced = parsed.error.details.reduce((acc: any, detail: any) => {
-          switch (detail['@type']) {
-            case 'type.googleapis.com/google.rpc.QuotaFailure': return {
-              ...acc,
-              violations: detail.violations,
-            };
-            case 'type.googleapis.com/google.rpc.RetryInfo': return {
-              ...acc,
-              retryDelay: detail.retryDelay.replace('s', '') * 1,
-            };
+      case 429:
+        const apiErrorInfoWithMessage = ApiErrorInfoSchemaWithMessage.safeParse(caught);
+        if (apiErrorInfoWithMessage.success) {
+          const apiError = new ApiError(apiErrorInfoWithMessage.data);
+          try {
+            const parsed = JSON.parse(apiError.message);
+            const reduced = parsed.error.details.reduce((acc: any, detail: any) => {
+              switch (detail['@type']) {
+                case 'type.googleapis.com/google.rpc.QuotaFailure': return {
+                  ...acc,
+                  violations: detail.violations,
+                };
+                case 'type.googleapis.com/google.rpc.RetryInfo': return {
+                  ...acc,
+                  retryDelay: detail.retryDelay.replace('s', '') * 1,
+                };
+              }
+            }, { retryDelay: 0, violations: [] } as {
+              // TODO: Update violations to be an actual type instead of this static nonsense.
+              retryDelay: number, violations: {
+                quotaMetric: 'generativelanguage.googleapis.com/generate_content_free_tier_requests',
+                quotaId: 'GenerateRequestsPerDayPerProjectPerModel-FreeTier',
+                quotaDimensions: { location: 'global', model: 'gemini-3-flash' },
+                quotaValue: '20'
+              }[],
+            });
+            return {
+              canRetry: true,
+              model,
+              retryTimeout: reduced.retryDelay,
+              status: 'rate-limitations',
+            }
+          } catch (e) {
+            setStatus('error', 'Gemini 429 error. Could not parse message as JSON.');
+            console.error(apiError, caught, e);
           }
-        }, { retryDelay: 0, violations: [] } as {
-          // TODO: Update violations to be an actual type instead of this static nonsense.
-          retryDelay: number, violations: {
-            quotaMetric: 'generativelanguage.googleapis.com/generate_content_free_tier_requests',
-            quotaId: 'GenerateRequestsPerDayPerProjectPerModel-FreeTier',
-            quotaDimensions: { location: 'global', model: 'gemini-3-flash' },
-            quotaValue: '20'
-          }[],
-        });
-        return {
-          canRetry: true,
-          model,
-          retryTimeout: reduced.retryDelay,
-          status: 'rate-limitations',
+          return {
+            canRetry: true,
+            model,
+            status: 'rate-limitations',
+          };
+        } else {
+          console.error(`Error is not compatible with ApiError.`);
         }
-      } catch (e) {
-        log('Gemini 429 error. Could not parse message as JSON.', 'error');
-        console.error(apiError, caught, e);
+      // TODO: In theory we can put in a 404 catcher and use it as a warning
+      // system that the gemini library needs a version update.
+      default: {
+        return {
+          canRetry: false,
+          model,
+          message: `Unexpected error status: ${apiErrorInfo.data.status}`,
+          status: 'unexpected',
+        }
       }
-      return {
-        canRetry: true,
-        model,
-        status: 'rate-limitations',
-      };
     }
   } else {
-    console.error(`Error is not an ApiErrorInfo object.`);
+    console.error(`Error is not an ApiErrorInfo object`)
   }
 
-  log(`Available error object keys:`, 'error');
+  setStatus('error', `Error response was difficult to parse. Available error object keys:`);
   console.error(Object.keys(caught))
 
   throw caught;

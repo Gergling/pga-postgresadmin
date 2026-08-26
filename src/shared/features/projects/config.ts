73494b4f8efe94e58f53d389be12e6b2791b850d
Greg <gregmichaeldavies@gmail.com>
2026-08-26 19:27:56 +0100

    refactor(projects): refactor project feature directory structure and exports

---
+++ src/shared/features/projects/config.ts
@@ -0,0 +1,5 @@
+import { configureValidOperationCodes } from "../llm";
+
+configureValidOperationCodes('project', [
+  'staged-commit-message'
+]);
--- src/shared/features/projects/index.ts
+++ src/shared/features/projects/index.ts
@@ -1,3 +1,4 @@
 export * from './config';
 export * from './utilities';
+export * from './schema';
 export * from './types';

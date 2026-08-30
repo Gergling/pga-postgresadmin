04672180b427ed47cd797d9e2635f55864a8a66a
Greg <gregmichaeldavies@gmail.com>
2026-08-30 13:37:53 +0100

    refactor(trpc): refactor rpcLog to support currying

--- src/main/config/trpc.ts
+++ src/main/config/trpc.ts
@@ -3,13 +3,24 @@ import { log, LogApi } from '../shared';
 
 export const tRPC = initTRPC.create({ isServer: true, transformer: undefined });
 
-export const rpcLog = <T>(
-  { input, path }: {
-    input?: string;
-    path: string;
-  }, callback: (logApi: LogApi) => Promise<T>
+type RpcLogProps = { input?: unknown; path: string; };
+export const callRpcLog = <T extends RpcLogProps, U extends Promise<unknown>>(
+  params: T, callback: (logApi: LogApi) => U
 ) => {
-  const txt = `RPC(${path})`;
-  if (input) return log(`${txt}: ${input}`, callback);
-  return log(`${txt}`, callback);
+  const txt = `RPC(${params.path})`;
+  if (typeof params.input === 'string') return log(
+    `${txt}: ${params.input}`, (logApi) => callback(logApi)
+  );
+  return log(`${txt}`, (logApi) => callback(logApi));
+}
+export const rpcLog = <T extends RpcLogProps, U>(
+  callback: (props: { params: T; logApi: LogApi; }) => Promise<U>,
+) => {
+  return (params: T): Promise<U> => {
+    const txt = `RPC(${params.path})`;
+    if (typeof params.input === 'string') return log(
+      `${txt}: ${params.input}`, (logApi) => callback({ params, logApi })
+    );
+    return log(`${txt}`, (logApi) => callback({ params, logApi }));
+  };
 }
